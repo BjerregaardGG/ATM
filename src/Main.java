450a7c7182b4873960e33f33b5e3d8237bef2c88
Telity <Nigu0001@Stud.kea.dk>
2025-02-18 11:11:15 +0100

    ATM main run

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final String TXT_QUIT = "q";
+    private static final String MSG_INPUT_CARDNUMBER = "Input card number (q=quit): ";
+    private static final String MSG_INPUT_PINCODE = "Input pin code: ";
+    private static final String MSG_INPUT_AMOUNT = "Input amount ($): ";
+    private static final String ERR_AUTHENTICATION = "Authentication failed. Please try again.";
+    private static final String ERR_INVALID_CARD = "Invalid card. Please try again.";
+    private static final String MSG_QUIT_RECEIVED = "ATM quit received";
+
+    public static void main(String[] args) {
+
+        ITransactionProcessor transactionProcessor = new TransactionProcessor();
+        ICardReader cardReader = new CardReader();
+        ICashDispenser cashDispenser = new CashDispenser();
+        ISecuritySystem securitySystem = new SecuritySystem();
+
+        boolean quit = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while(!quit) {
+            System.out.println("*******************************");
+            System.out.println("**   ATM Simulator Program   **");
+            System.out.println("*******************************");
+            System.out.println(MSG_INPUT_CARDNUMBER);
+            String cardNumber = scanner.next();
+
+            if(!cardNumber.equals(TXT_QUIT)) {
+                System.out.println(MSG_INPUT_PINCODE);
+                String pinCode = scanner.next();
+
+                ICard card = cardReader.validateCard(cardNumber);
+                if(card != null) {
+                    IAccount account = card.getAccount();
+                    if(securitySystem.authenticate(card.getPin(), pinCode)){
+
+                        System.out.println(MSG_INPUT_AMOUNT);
+                        double amount = Double.parseDouble(scanner.next());
+
+                        transactionProcessor.processTransaction(account, amount);
+                        cashDispenser.dispenseCash(amount);
+                    } else {
+                        System.out.println(ERR_AUTHENTICATION);
+                    }
+                } else {
+                    System.out.println(ERR_INVALID_CARD);
+                }
+            } else {
+                quit = true;
+                System.out.println(MSG_QUIT_RECEIVED);
+            }
         }
     }
 }
